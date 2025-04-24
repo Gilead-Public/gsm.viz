03a32ab4608101f6f6285885024785a32703461a
samussiah <samussiah@users.noreply.github.com>
2025-04-24 18:51:45 +0000

    Deploying to gh-pages from @ Gilead-BioStats/gsm.viz@ef380b19f4242ddc3f7e8f19c302484b6366d136 🚀

--- index.js
+++ index.js
@@ -20247,6 +20247,7 @@ var gsmViz = (() => {
         "Denominator",
         "Metric",
         "Score",
+        "ScoreExtra",
         "Abbreviation"
       ]
     }
@@ -20283,6 +20284,7 @@ var gsmViz = (() => {
       "Metric",
       "Outcome",
       "Score",
+      "ScoreExtra",
       "Abbreviation"
     ],
     properties: {
@@ -20327,6 +20329,13 @@ var gsmViz = (() => {
         key: false
       },
       Score: {
+        title: "Metric Score",
+        description: "Analysis method of metric",
+        type: "string",
+        required: false,
+        key: false
+      },
+      ScoreExtra: {
         title: "Metric Score",
         description: "Analysis method of metric",
         type: "string",
@@ -20391,6 +20400,7 @@ var gsmViz = (() => {
         "Denominator",
         "Metric",
         "Score",
+        "ScoreExtra",
         "Flag",
         "SnapshotDate"
       ],
@@ -20447,6 +20457,13 @@ var gsmViz = (() => {
         Score: {
           title: "Metric Score",
           description: "Metric score",
+          type: "number",
+          required: true,
+          key: false
+        },
+        ScoreExtra: {
+          title: "Metric Score Extra",
+          description: "Metric Score Extra",
           type: "number",
           required: true,
           key: false
@@ -21341,6 +21358,7 @@ var gsmViz = (() => {
   function formatMetricTooltipLabel(result, config) {
     const tooltipKeys = {
       Score: config.Score || "Score",
+      ScoreExtra: config.ScoreExtra || "ScoreExtra",
       Metric: config.Metric || "Metric",
       Numerator: config.Numerator || "Numerator",
       Denominator: config.Denominator || "Denominator"
