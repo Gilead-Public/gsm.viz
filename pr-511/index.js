cda0ee9cd37ce2758b2de3afc3ca7d659755e564
samussiah <samussiah@users.noreply.github.com>
2026-06-11 20:37:57 +0000

    Deploying to gh-pages from @ Gilead-BioStats/gsm.viz@dc02d54f6e9b5cbb4b8f74e1e781dfa618d982e0 🚀

--- pr-511/index.js
+++ pr-511/index.js
@@ -22710,11 +22710,31 @@ var gsmViz = (() => {
     chart.update();
   }
 
+  // src/bars/defaultFilename.js
+  function toFilename(str) {
+    return str.toLowerCase().replace(/[^a-z0-9\s-]/g, "").trim().replace(/\s+/g, "-");
+  }
+  function defaultFilename(spec) {
+    const { labels = {}, scales: scales2 = {}, mapping = {} } = spec || {};
+    if (labels.title) {
+      return toFilename(labels.title) + ".png";
+    }
+    if (scales2.fill?.label && scales2.x?.label) {
+      return toFilename(scales2.fill.label) + "-by-" + toFilename(scales2.x.label) + ".png";
+    }
+    if (mapping.x) {
+      const prefix = mapping.fill ? toFilename(mapping.fill) + "-by-" : "";
+      return prefix + toFilename(mapping.x) + ".png";
+    }
+    return "bars.png";
+  }
+
   // src/bars/exportImage.js
-  function exportImage(chart, filename = "bars.png") {
+  function exportImage(chart, filename) {
+    const name = filename !== void 0 ? filename : defaultFilename(chart.data?._spec_);
     const dataURL = chart.toBase64Image();
     const a = document.createElement("a");
-    a.download = filename;
+    a.download = name;
     a.href = dataURL;
     document.body.appendChild(a);
     a.click();
